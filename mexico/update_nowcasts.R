972db6c099a036ac545a4eaa2092d7458e0317df
tozanni <tozanni@gmail.com>
2020-05-08 00:36:36 +0000

    Resultados con corte al 06 de Mayo de 2020.
    Fuente de datos mapa SINAVI.

--- mexico/update_nowcasts.R
+++ mexico/update_nowcasts.R
@@ -1,56 +1,19 @@
 # Packages -----------------------------------------------------------------
 
-require(EpiNow)
-require(NCoVUtils)
-require(furrr)
-require(future)
-require(dplyr)
-require(tidyr)
-require(magrittr)
+require(EpiNow, quietly = TRUE)
+require(NCoVUtils, quietly = TRUE)
+require(furrr, quietly = TRUE)
+require(future, quietly = TRUE)
+require(dplyr, quietly = TRUE)
+require(tidyr, quietly = TRUE)
+require(magrittr, quietly = TRUE)
+require(future.apply, quietly = TRUE)
+require(fable, quietly = TRUE)
+require(fabletools, quietly = TRUE)
+require(feasts, quietly = TRUE)
+require(urca, quietly = TRUE)
 
 # Get cases ---------------------------------------------------------------
-
-###  Mexico
-
-get_mexico_linelist <- function() {
-  #Updated 2020-04-28
-  path <- "https://raw.githubusercontent.com/marianarf/covid19_mexico_analysis/master/latest.csv"
-  
-  #Set up cache
-  ch <- memoise::cache_filesystem(".cache")
-  mem_read <- memoise::memoise(readr::read_csv, cache = ch)
-  #mem_read <- memoise::memoise(readr::read_csv)
-  
-  cases <- mem_read(path) %>%
-    #Quitar acentos en los nombres de los estados
-    dplyr::mutate(Region=stringi::stri_trans_general(str = Region, id = "Latin-ASCII")) %>%
-    
-    #Se les olvido lo de CDMX
-    dplyr::mutate(Region = ifelse(Region == "DISTRITO FEDERAL" , "CIUDAD DE MEXICO", Region)) %>%
-    
-    #Date Symptoms, nuevo formato
-    dplyr::mutate(Date_Symptoms=as.Date(Date_Symptoms, format = "%d-%m-%Y")) %>%
-    
-    # Usamos Date_Confirmed, nuev formato
-    dplyr::mutate(Date_Confirmed=as.Date(Date_Confirmed, format = "%d-%m-%Y")) %>%
-    #Fix de MDY para pendientes de 14-04, 15-04 y 16-04
-    #dplyr::mutate(date2 = as.Date.numeric(
-    #  ifelse( is.na(Date_Confirmed), 
-    #          as.Date(Date_Confirmed, format = "%m-%d-%Y"), Date_Confirmed ),
-    #  origin="1970-01-01")) %>%
-    #dplyr::mutate(Date_Confirmed = date2 ) %>%
-    
-    #Crear columna local o importado
-    dplyr::mutate(import_status = ifelse(Origin == "Local" , "local", "imported")) %>%
-    
-    ##Linelist
-    dplyr::mutate(report_delay = as.numeric(Date_Confirmed - Date_Symptoms))  %>%
-    dplyr::rename(date_onset = Date_Symptoms, 
-                  date_confirm = Date_Confirmed) %>%
-    dplyr::select(date_onset, date_confirm, report_delay, import_status)
-  
-  return(cases)
-}
 
 ###  Mexico (Regional)
 get_mexico_regional_cases <- function() {
@@ -102,9 +65,14 @@ get_mexico_regional_cases <- function() {
 
 NCoVUtils::reset_cache()
 
-cases <- get_mexico_regional_cases() %>% 
-  dplyr::filter(import_status  == "local") %>%
-  dplyr::rename(region = Region)
+# Opcion directo desde archivo subido
+# Se quita el primer registro porque sale negativo por el shift
+cases <- readr::read_csv('/home/covid/casos_060520.txt') %>% 
+  dplyr::filter( cases > 0 )
+
+#cases <- get_mexico_regional_cases() %>% 
+#  dplyr::filter(import_status  == "local") %>%
+#  dplyr::rename(region = Region)
 
 #cases %>% View()
 
@@ -117,13 +85,106 @@ cases <- get_mexico_regional_cases() %>%
 
 # Get linelist ------------------------------------------------------------
 
-#linelist <- get_mexico_linelist()
+get_international_linelist_custom <- function(countries = NULL, cities = NULL, provinces = NULL, clean = TRUE) {
+  
+  country <- NULL; city <- NULL; travel_history_location <- NULL;
+  travel_history_dates <- NULL; date_confirmation <- NULL;
+  date_onset_symptoms <- NULL; date_confirm <- NULL;
+  date_onset <- NULL; report_delay <- NULL; import_status <- NULL;
+  
+  message("Downloading linelist data")
+  
+  
+  ch <- memoise::cache_filesystem(".cache")
+  
+  url <- "https://raw.githubusercontent.com/beoutbreakprepared/nCoV2019/master/latest_data/latestdata.csv"
+  
+  mem_read <- memoise::memoise(readr::read_csv, cache = ch)
+  linelist <- suppressWarnings(
+    suppressMessages(
+      try(R.utils::withTimeout(mem_read(url) %>%
+                                 tibble::as_tibble(), timeout = 15, onTimeout = "error"),
+          silent = TRUE)
+    )
+  )
+  
+  if (any(class(linelist) %in% "try-error")) {
+    warning("Could not access linelist source. Using the NCoVUtils cache, this may not be up to date.
+    See the git history to confirm last cache date.")
+    
+    url <- "https://raw.githubusercontent.com/epiforecasts/NCoVUtils/master/data-raw/linelist.csv"
+    
+    linelist <- suppressWarnings(
+      suppressMessages(
+        mem_read(url) %>%
+          tibble::as_tibble()
+      )
+    )
+  }
+  
+  if (!is.null(countries)) {
+    linelist <- linelist %>%
+      dplyr::filter(country %in% countries)
+  }
+  
+  if (!is.null(cities)) {
+    linelist <- linelist %>%
+      dplyr::filter(city %in% cities)
+  }
+  
+  if (!is.null(provinces)) {
+    linelist <- linelist %>%
+      dplyr::filter(province %in% provinces)
+  }
+  
+  if (clean) {
+    linelist <- linelist %>%
+      dplyr::mutate(travel_history_location = ifelse(travel_history_location %in% "", NA, travel_history_location),
+                    travel_history_dates = ifelse(travel_history_dates %in% "", NA, travel_history_dates)) %>%
+      
+      dplyr::mutate(import_status =
+            
+                    dplyr::if_else(!is.na(travel_history_location) | !is.na(travel_history_dates), "imported", "local"),
+                    date_confirm = lubridate::dmy(date_confirmation),
+                    date_onset = lubridate::dmy(date_onset_symptoms),
+                    report_delay =
+                      as.integer(as.Date(date_confirm) - as.Date(date_onset))) %>%
+      
+      dplyr::select(date_onset, date_confirm, report_delay, import_status, country) %>%
+      tidyr::drop_na(date_confirm)
+    
+  }
+  
+  return(linelist)
+  
+}
 
-linelist <- NCoVUtils::get_international_linelist()
+## Version custom arreglada por 
+#print("Custom international linelist... ")
+#linelist <- get_international_linelist_custom(clean=TRUE)
+#print("DONE")
+
+#saveRDS(linelist,'')
+#saveRDS(linelist, file = "/home/covid/my_linelist.rds")
+
+print("Linelist procesada previamente...")
+linelist <- readRDS("/home/covid/my_linelist.rds")
+print("DONE Reading RDS")
+
+#ll2 %>% View()
+
 
 # Set up cores -----------------------------------------------------
 
+if (!interactive()){
+  print("Future.fork, enable=TRUE")
+  options(future.fork.enable = TRUE)
+}
+
 future::plan("multiprocess", workers = future::availableCores())
+
+data.table::setDTthreads(threads = 1)
+
 
 # Run pipeline ----------------------------------------------------
 
@@ -133,8 +194,25 @@ EpiNow::regional_rt_pipeline(
   regional_delay = FALSE,
   target_folder = "mexico/regional",
   case_limit = 10,
-  verbose = TRUE
+  verbose = TRUE,
+
+  horizon = 14,
+  approx_delay = TRUE,
+  report_forecast = TRUE,
+  forecast_model = function(...) {
+    EpiSoon::fable_model(model = fabletools::combination_model(fable::RW(y ~ drift()), fable::ETS(y), 
+                                                               fable::NAIVE(y),
+                                                               cmbn_args = list(weights = "inv_var")), ...)
+  }
+  
 )
+
+## Cambio merge_onsets a FALSE
+#' @param linelist A dataframe of of cases (by row) containing the following variables:
+#' `import_status` (values "local" and "imported"), `date_onset`, `date_confirm`, `report_delay`, and `region`. If a national linelist is not available a proxy linelist may be 
+#' used but in this case `merge_onsets` should be set to `FALSE`.
+
+
 
 # Summarise results -------------------------------------------------------
 
